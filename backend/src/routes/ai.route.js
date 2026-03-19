d5df38a1bda03cfa004fba2f68c2b9405218cd74
Hamna Tariq <hamnatariq663@gmail.com>
2026-03-19 22:31:40 +0500

    Add frontend folder from main

--- backend/src/routes/ai.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express"
-import getReview from "../controllers/ai.controller.js";
-const router = express.Router()
-
-router.post("/get-review", getReview);
-
-
-export default router
