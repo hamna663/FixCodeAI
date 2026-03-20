c5c88bf2c62de139ccf259391bed52c90d2786fb
Hamna Tariq <hamnatariq663@gmail.com>
2026-03-20 13:38:45 +0500

    Add serverless-http dependency to package.json and package-lock.json

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,9 @@
 import app from './src/app.js'
 import dotenv from "dotenv";
 dotenv.config();
-
+import serverless from 'serverless-http';
 
 const port = process.env.PORT;
 
 
-app.listen(port, () =>
-    console.log(`Server running on http://localhost:${port} 🔥`)
-);
+export default serverless(app);
