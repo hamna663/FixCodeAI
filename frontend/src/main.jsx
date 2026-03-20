36416b9372bde4434b56545b814ebc3664795307
Hamna Tariq <hamnatariq663@gmail.com>
2026-03-20 12:47:08 +0500

    Add @vercel/analytics dependency and integrate Analytics component in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
+import { Analytics } from "@vercel/analytics/next"
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics />
     <App />
   </StrictMode>,
 )
